7e86f4f190cff371d748a6a61e56b61ed87d6abe
Mateus Correia <mateuscorreia.dev@gmail.com>
2024-03-17 18:43:31 -0300

    created the nav function using the hook useRouter to create a function for the active links

---
+++ pages/configurations.js
@@ -0,0 +1,5 @@
+import Layout from "@/components/Layout";
+
+export default function Configuration() {
+  return <Layout>Minhas configurações</Layout>;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,5 @@
-import { useSession, signIn, signOut } from "next-auth/react"
+import Layout from "@/components/Layout";
 
 export default function Home() {
-  const { data: session } = useSession()
-  if (!session) {
-    return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          <button onClick={ () => signIn('google')} className="bg-white p-2 px-4 rounded-md">Acessar com Google</button>
-        </div>
-      </div>
-    )
-  }
-
-  return (
-    <div>Logado em {session.user.email}</div>
-  )
+  return <Layout>Meu dashboard</Layout>;
 }
