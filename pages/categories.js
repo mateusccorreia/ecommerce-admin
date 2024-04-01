99169fd7f6c83369b9adb6899786b5b11ed9122e
Mateus Correia <mateuscorreia.dev@gmail.com>
2024-04-01 06:11:51 -0300

    added the async function for the category section

--- pages/categories.js
+++ pages/categories.js
@@ -1,14 +1,27 @@
 import Layout from "@/components/Layout";
+import axios from "axios";
+import { useState } from "react";
 
 export default function Categories() {
-  function saveCategory() {}
+  const [name, setName] = useState("");
+  async function saveCategory(ev) {
+    ev.preventDefault();
+    await axios.post("/api/categories", { name });
+    setName("");
+  }
 
   return (
     <Layout>
       <h1>Categorias</h1>
       <label>Nova categoria</label>
       <form onSubmit={saveCategory} className="flex gap-1">
-        <input className="mb-0" type="text" placeholder={"nome da categoria"} />
+        <input
+          className="mb-0"
+          type="text"
+          placeholder={"nome da categoria"}
+          onChange={(ev) => setName(ev.target.value)}
+          value={name}
+        />
         <button type="submit" className="btn-purple py-1">
           Salvar
         </button>
