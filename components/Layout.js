b9a56e1c2bbf5678a1787354fc8527ee2f56c6bf
Mateus Correia <mateuscorreia.dev@gmail.com>
2024-06-22 22:38:13 -0300

    created the responsive menu

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
 import { useSession, signIn, signOut } from "next-auth/react";
 import Nav from "@/components/Nav";
 import { useState } from "react";
+import Logo from "./Logo";
 
 export default function Layout({ children }) {
   const [showNav, setShowNav] = useState(false);
@@ -22,27 +23,31 @@ export default function Layout({ children }) {
 
   return (
     <div className="bg-bgGray min-h-screen">
-      <button onClick={() => setShowNav(true)}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="size-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-          />
-        </svg>
-      </button>
+      <div className="block md:hidden flex items-center">
+        <button onClick={() => setShowNav(true)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </button>
+        <div className="flex grow justify-center mr-6">
+          <Logo />
+        </div>
+      </div>
+
       <div className=" flex">
         <Nav show={showNav} />
-        <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">
-          {children}
-        </div>
+        <div className="flex-grow p-4">{children}</div>
       </div>
     </div>
   );
